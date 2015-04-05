8ef1a430e9111fc5139c4eaf1f22f66c422b3a61
Karl Horky <karl.horky@gmail.com>
2015-04-05 15:06:59 +0200

    Fix linting errors

--- index.js
+++ index.js
@@ -3,19 +3,21 @@
 
 var
   program = require('commander'),
-  chalk   = require('chalk');
+  chalk   = require('chalk'),
+
+  publishCommand = require('./commands/publish')(program),
+  serveCommand   = require('./commands/serve')(program);
 
 program
   .version('1.0.0');
 
-var
-  publish = require('./commands/publish')(program),
-  serve   = require('./commands/serve')(program);
+publishCommand(program);
+serveCommand(program);
 
 program
   .command('*')
   .description('Prints help')
-  .action(function(options) {
+  .action(function() {
     console.error(chalk.red('\n  Invalid command. Please use one of the following:'));
     program.outputHelp();
   });
