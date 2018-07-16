de45d609c253ce4047270a3df5f5e31c77912334
Logan Volkers <logan@saasquat.ch>
2018-07-16 14:22:02 -0700

    LV: Added babel for use with Ink and older node versions

--- src/commands/components/checkbox.js
+++ src/commands/components/checkbox.js
@@ -2,11 +2,7 @@ const { h, Color } = require('ink');
 
 function CheckBox({ checkedCharacter, uncheckedCharacter, isChecked }) {
 	const mark = isChecked === true ? checkedCharacter : uncheckedCharacter;
-	return h(
-		Color,
-		{ green: true },
-		` ${mark}  `
-	);
+	return <Color green={true}> {mark}  </Color>
 }
 
 module.exports = CheckBox;
--- src/commands/components/cursor.js
+++ src/commands/components/cursor.js
@@ -2,11 +2,7 @@ const { h } = require('ink');
 
 function Cursor({ cursorCharacter, isActive }) {
 	const c = isActive === true ? `${cursorCharacter} ` : '\u00A0'.repeat(cursorCharacter.length + 1);
-	return h(
-		'span',
-		null,
-		c
-	);
+	return <span>{c}</span>;
 }
 
 module.exports = Cursor;
