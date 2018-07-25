d60bb6284bf9d2958d98044dfa186c823aa4bc87
Jiaqi Huang <huangjiaqi88@gmail.com>
2018-07-25 10:31:25 -0700

    JH: fix default domain

--- src/commands/query/query.js
+++ src/commands/query/query.js
@@ -138,7 +138,7 @@ const Query = ({ domain, tenant, authToken }) => {
                     }
                   }
                 }
-              } 
+              }
             }
           }
         `
